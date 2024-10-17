35fa533926e3b1631a71d7cff85c5ea73f12f6b8
VijayVadaboina <vijayvvadaboina@gmail.com>
2024-10-17 15:59:59 +0530

    fetching recipe details from spoonacular api & store in setfood state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Search from "./components/Search";
+import { useState } from "react";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
   return (
     <div>
-      <Search></Search>
+      <Search foodData={foodData} setFoodData={setFoodData} />
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import.meta.env.VITE_RECIPE_URL;
+// import.meta.env.VITE_APIKEY;
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("");
-
+  useEffect(() => {
+    async function fetchFoodRecipeDetails() {
+      const res = await fetch(
+        `${import.meta.env.VITE_RECIPE_URL}?query=${query}&apiKey${
+          import.meta.env.VITE_APIKEY
+        }`
+      );
+      const data = res.json();
+      setFoodData(data.results);
+    }
+  }, [query]);
   return (
     <div>
       <input
